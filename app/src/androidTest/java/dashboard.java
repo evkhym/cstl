1e4b87ac9b02e615a58cf6f509f4e29ffc39b41a
degromscherzer <ekhym24@groton.org>
2022-08-05 06:18:21 -0400

    Buttons

--- app/src/androidTest/java/dashboard.java
+++ app/src/androidTest/java/dashboard.java
@@ -1,17 +1,30 @@
 package evan.khym.ecocollect;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class Dashboard extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
-    }
-}
+
+        Button dash = findViewById(R.id.button5);
+
+        dash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(Dashboard.this, Dashboard.class);
+                startActivity(it);
+//                finish();
+            }
+        });
--- app/src/main/java/evan/khym/ecocollect/Login.java
+++ app/src/main/java/evan/khym/ecocollect/Login.java
@@ -1,7 +1,9 @@
 package evan.khym.ecocollect;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,11 +12,28 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        Button login2 = findViewById(R.id.buttongologin);
 
-    }
-}
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(Login.this, Login.class);
+                startActivity(it);
+//                finish();
+            }
+
+            Button login1 = findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent it = new Intent(Login.this, Login.class);
+                    startActivity(it);
+//                finish();
+        });
